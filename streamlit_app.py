71db606c18c220f7542b418bd851abf9d0a6b6f2
SmitKotadiaBrilworks <156425528+SmitKotadiaBrilworks@users.noreply.github.com>
2026-06-23 17:33:22 +0530

    Fix connection string syntax in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write("The name on smoothie will be:", name_on_order)
 
 # st.write("your favorite fruit is:", option)
 
-cnx = st.connection("snowflake)
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True
